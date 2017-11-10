c502506516b7f709db06069750c0f8e9cd67b025
Lu Nelson <lunelson@gmail.com>
2017-11-10 11:19:59 +0100

    basic working rollup

--- serve-js.js
+++ serve-js.js
@@ -1,34 +1,3 @@
-const { join, relative, resolve, extname } = require('path');
-const { stat } = require('fs');
-
-///
-/// SUBWARE
-///
-
-module.exports = function subWare(baseDir, changeTimes) {
-  const renderCache = {};
-  const renderTimes = {};
-  return function(absFile, res, next) {
-    const ext = extname(absFile);
-    const now = Date.now();
-    stat(absFile, (err, stats) => {
-      // if requested file exists, bail out
-      // otherwise replace the extension and query that
-      if (err || !stats.isFile()) return next();
-      if (!(absFile in renderCache) || renderTimes[absFile] < changeTimes[ext]) {
-        // process this shit
-      }
-    });
-    console.log(
-      `${ext} file -- \n changed: ${changeTimes[ext]} \n rendered: ${
-        renderTimes[absFile]
-      } \n served: ${now}`
-    );
-    res.setHeader('Content-Type', 'text/javascript');
-    res.end(renderCache[absFile]);
-  };
-};
-
 //   _
 //   (_)
 //    _ ___
@@ -38,43 +7,60 @@ module.exports = function subWare(baseDir, changeTimes) {
 //  _/ |
 // |__/
 
+const { join, relative, resolve, extname } = require('path');
+const { stat } = require('fs');
+const { replaceExt } = require('./serve-utils');
 const Rollup = require('rollup');
-const nodeResolve = require('rollup-plugin-node-resolve');
-const commonJS = require('rollup-plugin-commonjs');
+const nodeResolvePlugin = require('rollup-plugin-node-resolve');
+const commonJsPlugin = require('rollup-plugin-commonjs');
+const babelPlugin = require('rollup-plugin-babel');
+const replacePlugin = require('rollup-plugin-replace');
 
-function jsWare(changeTimes) {
-  const ext = '.js',
-    renderCache = {},
-    bundleCache = {},
-    config = {
-      external: [],
-      plugins: [
-        nodeResolve(),
-        commonJS()
-        // babel(),
-        // replace({
-        //   ENV: JSON.stringify(process.env.NODE_ENV || "development")
-        // })
-      ]
-    };
-  let renderTime = 0;
+///
+/// SRCWARE
+///
 
-  return function(req, res, next) {
-    const filename = join(__dirname, req.url);
-    stat(filename, (err, stats) => {
-      if (!stats.isFile()) return next();
-      // const now = Date.now();
-      if (renderTime < changeTimes[ext]) {
+module.exports = function subWare(baseDir, changeTimes) {
+  const renderCache = {};
+  const renderTimes = {};
+  const bundleCache = {};
+  const srcExt = '.js';
+  const config = {
+    external: [],
+    plugins: [
+      nodeResolvePlugin(),
+      commonJsPlugin(),
+      babelPlugin(),
+      replacePlugin({
+        ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+      })
+    ]
+  };
+  // NB: we don't check reqFile vs srcFile here; they are the same
+  return function(srcFile, res, next) {
+    stat(srcFile, (err, stats) => {
+      // bail, if srcFile does not exist
+      if (err || !stats.isFile()) return next();
+      const now = Date.now();
+      // if renderCache invalid, re-render and update renderTime
+      if (!(srcFile in renderCache) || renderTimes[srcFile] < changeTimes[srcExt]) {
         Rollup.rollup(
-          Object.assign({}, config, { input: filename, cache: bundleCache[req.url] })
+          Object.assign({}, config, {
+            input: srcFile,
+            cache: bundleCache[srcFile]
+          })
         ).then(
           bundle => {
-            bundleCache[req.url] = bundle;
-            renderCache[req.url] = bundle.generate({
-              format: 'es',
-              sourcemap: 'inline'
-            }).code;
-            renderTime = Date.now();
+            bundleCache[srcFile] = bundle;
+            bundle
+              .generate({
+                format: 'es',
+                sourcemap: 'inline'
+              })
+              .then(data => {
+                renderCache[srcFile] = data.code;
+                renderTimes[srcFile] = now;
+              });
           },
           err => {
             if (err.code === 'PARSE_ERROR') {
@@ -99,9 +85,77 @@ function jsWare(changeTimes) {
           }
         );
       }
+      console.log(
+        `${srcExt} file\n changed: ${changeTimes['.pug']} \n rendered: ${
+          renderTimes[srcFile]
+        } \n served: ${now}`
+      );
       res.setHeader('Content-Type', 'text/javascript');
-      // res.end('IS THIS THING ON???');
-      res.end(renderCache[req.url]);
+      res.end(renderCache[srcFile]);
     });
   };
-}
+};
+
+// function jsWare(changeTimes) {
+//   const ext = '.js',
+//     renderCache = {},
+//     bundleCache = {},
+//     config = {
+//       external: [],
+//       plugins: [
+//         nodeResolvePlugin(),
+//         commonJsPlugin()
+//         // babel(),
+//         // replace({
+//         //   ENV: JSON.stringify(process.env.NODE_ENV || "development")
+//         // })
+//       ]
+//     };
+//   let renderTime = 0;
+
+//   return function(req, res, next) {
+//     const filename = join(__dirname, req.url);
+//     stat(filename, (err, stats) => {
+//       if (!stats.isFile()) return next();
+//       // const now = Date.now();
+//       if (renderTime < changeTimes[ext]) {
+//         Rollup.rollup(
+//           Object.assign({}, config, { input: filename, cache: bundleCache[req.url] })
+//         ).then(
+//           bundle => {
+//             bundleCache[req.url] = bundle;
+//             renderCache[req.url] = bundle.generate({
+//               format: 'es',
+//               sourcemap: 'inline'
+//             }).code;
+//             renderTime = Date.now();
+//           },
+//           err => {
+//             if (err.code === 'PARSE_ERROR') {
+//               console.error(
+//                 '%s:%d:%d: %s',
+//                 relative(__dirname, err.loc.file),
+//                 err.loc.line,
+//                 err.loc.column,
+//                 err.message
+//               );
+//               console.error();
+//               console.error(err.frame);
+//               console.error();
+//               res.writeHead(500);
+//               res.end();
+//             } else if (err.code === 'UNRESOLVED_ENTRY') {
+//               // Pass 404s on to the next middleware
+//               next();
+//             } else {
+//               next(err);
+//             }
+//           }
+//         );
+//       }
+//       res.setHeader('Content-Type', 'text/javascript');
+//       // res.end('IS THIS THING ON???');
+//       res.end(renderCache[req.url]);
+//     });
+//   };
+// }
--- serve-pug.js
+++ serve-pug.js
@@ -6,9 +6,8 @@
 // |_|          |___/
 
 const { join, relative, resolve, extname } = require('path');
-const { stat, readFile } = require('fs');
+const { stat } = require('fs');
 const { replaceExt } = require('./serve-utils');
-
 const Pug = require('pug');
 
 /*
@@ -28,15 +27,15 @@ module.exports = function(baseDir, changeTimes) {
   const locals = {};
   return function(reqFile, res, next) {
     stat(reqFile, (err, stats) => {
-      // bounce, if reqFile actually exists
+      // bail, if reqFile actually exists
       if (!err && stats.isFile()) return next();
       const srcFile = replaceExt(reqFile, srcExt);
       stat(srcFile, (err, stats) => {
-        // bounce, if srcFile does not exist
+        // bail, if srcFile does not exist
         if (err || !stats.isFile()) return next();
         const now = Date.now();
+        // if renderCache invalid, re-render and update renderTime
         if (!(srcFile in renderCache) || renderTimes[srcFile] < changeTimes[srcExt]) {
-          // process this shit
           renderCache[srcFile] = Pug.renderFile(
             srcFile,
             Object.assign({}, locals, {
