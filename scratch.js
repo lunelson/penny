3de3ab46c924c0c061deb22edd6c7b32eb1e008f
Lu Nelson <lunelson@gmail.com>
2018-01-12 10:56:05 +0100

    change debug to dump (debug is a reserved flag)

---
+++ scratch.js
@@ -0,0 +1,44 @@
+const url = require('url');
+const path = require('path');
+
+const basedir = '/Users/lunelson/Git/packages/penny/test';
+const filename = '/Users/lunelson/Git/packages/penny/test/util/pug/props/index.pug';
+
+// COMPUTED locals
+const dirname = path.dirname(filename);
+const srcPath = path.relative(basedir, filename); //?
+const reqPath = '/'+srcPath.replace(/\.pug$/, '.html'); //?
+// const absPath = path.join(basedir, '/'+srcPath); //?
+
+__filename; //?
+__dirname; //?
+
+/* ======== */
+filename; //?
+path.dirname(filename); //?
+path.basename(filename); //?
+url.parse(path.relative(basedir, filename)).pathname; //?
+
+const pug = require('pug');
+const _ = require('lodash');
+
+function getFilename() { return this.filename };
+var locals = {};
+locals.getFilename = getFilename.bind(locals);
+const fileA = `
+pre!= getFilename()
+`;
+const fileB = `
+- foobar = 'fuck this'
+- locals.shit = 'crap'
+pre!= foobar
+pre!= getFilename()
+`;
+var html = pug.render(fileA, Object.assign(_.clone(locals), {filename: 'hello world'})); //?
+// var html = pug.render(fileB, Object.assign(locals, {filename: 'hello world'})); //?
+var html = pug.render(fileB, Object.assign(_.clone(locals), {filename: 'hello world'})); //?
+locals; //?
+
+// require.resolve('./index-api.js', { paths: __filename});
+const testPath = path.join(__dirname, './index-api.js') //?
+require.resolve(testPath); //?
