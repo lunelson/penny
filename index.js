ba2c27376b9e39257ce4f6f6c493cf229ffad909
Lu Nelson <lunelson@gmail.com>
2018-02-14 19:43:58 +0100

    deprecate unneeded files; break some shit

--- index.js
+++ index.js
@@ -3,39 +3,49 @@ const cosmiconfig = require('cosmiconfig');
 const doServe = require('./lib/serve');
 const doBuild = require('./lib/build');
 
-const stopDir = process.cwd();
-const rcLoader = cosmiconfig('penny', { stopDir, rcExtensions: true })
-  .load(stopDir)
-  .then((result) => result ? result.config : Object.create(null));
-
-const rcDefaults = {
-  // linting: false,
-  // caching: true, // hook this up to the caching in serve.js
-  // data: '', // WIP; could also be called 'locals'
-  browsers: ['>1%'],
-  eslint: false,
-  // stylelint: false,
-  reqSrcExt: { // sourceTypes | sources | sourceMatching
-    '.html': '.pug',
-    '.css': '.scss',
-    '.js': '.js'
-  }
-};
+// const stopDir = process.cwd();
+const rcLoader = cosmiconfig('penny', { stopDir: process.cwd(), rcExtensions: true });
 
 function rcCombine(defaults, options) {
   return Object.assign(defaults, options);
 }
 
+const rcDefaults = {
+  browsers: ['>1%'],
+  eslint: false,
+  reqSrcExt: { // TODO: pluralize
+    '.html': '.pug',
+    '.css': '.scss',
+    '.js': '.js'
+  },
+
+  /*
+  TODO: potential options
+
+  linting: true, // globally disable eslint + stylelint
+  caching: true, // whether serve.js should use the renderCache
+  data: '', // WIP; could also be called 'locals'
+  stylelint: false,
+
+  */
+};
+
 function serve(srcDir, isDev) {
-  rcLoader.then((rcOptions) => {
-    doServe(srcDir, isDev, rcCombine(rcDefaults, rcOptions));
-  });
+  rcLoader
+    .load(srcDir)
+    .then((result) => result ? result.config : Object.create(null))
+    .then((rcOptions) => {
+      doServe(srcDir, isDev, rcCombine(rcDefaults, rcOptions));
+    });
 }
 
 function build(srcDir, outDir) {
-  rcLoader.then((rcOptions) => {
-    doBuild(srcDir, outDir, rcCombine(rcDefaults, rcOptions));
-  });
+  rcLoader
+    .load(srcDir)
+    .then((result) => result ? result.config : Object.create(null))
+    .then((rcOptions) => {
+      doBuild(srcDir, outDir, rcCombine(rcDefaults, rcOptions));
+    });
 }
 
 module.exports = { serve, build };
