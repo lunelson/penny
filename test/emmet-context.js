d9fbc8d30f2143fb108159660da08578ed1cfab5
Lu Nelson <lunelson@gmail.com>
2018-05-09 09:40:55 +0200

    more package changes, sketches for new features
    # Conflicts:
    #	package-lock.json
    #	package.json

--- test/emmet-context.js
+++ test/emmet-context.js
@@ -1,15 +1,19 @@
 import parse from '@emmetio/abbreviation';
 import Profile from '@emmetio/output-profile';
 import format from '@emmetio/markup-formatters';
+import transform from '@emmetio/html-transform';
+
 import pug from 'pug';
 
 const pugProfile = new Profile({ indent: '  ' });
 
-const abbr = parse('ul#test>li.tester'+'>block');
-console.log(abbr);
+// const abbr = parse('table>.row>.col');
+const abbr = transform(parse('ul>.item>block'));
+// console.log(abbr);
+console.log(format(abbr, pugProfile, 'html'));
 console.log(format(abbr, pugProfile, 'pug'));
 
-console.log('mixin _abbr\n'+format(abbr, pugProfile, 'pug').replace(/^(.)/mg, '  $1'));
+// console.log('mixin _abbr\n'+format(abbr, pugProfile, 'pug').replace(/^(.)/mg, '  $1'));
 
 const testPugString = `
 ${'mixin _wrap\n'+format(abbr, pugProfile, 'pug').replace(/^(.)/mg, '  $1')}
@@ -18,4 +22,5 @@ ${'mixin _wrap\n'+format(abbr, pugProfile, 'pug').replace(/^(.)/mg, '  $1')}
 `;
 
 console.log(testPugString);
-console.log(pug.render(testPugString));
+console.log(pug.compile(testPugString));
+console.log(format(transform(parse('.outer>.inner*'), pug.render('h1 hello world')), pugProfile, 'html'));
