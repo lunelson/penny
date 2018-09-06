93a4bf9d7a122f82574983dc824ebaa5d815ff73
Lu Nelson <lunelson@gmail.com>
2018-09-06 20:54:59 +0200

    more adjustment

--- index.js
+++ index.js
@@ -15,21 +15,21 @@ const defaults = {
   browsers: ['>1%'],
   logLevel: 'warn', // [error, info, warn, debug, trace] (ascending verbosity)
   useHTTPS: false,
-  pubDirName: '_root',
+  // pubDirName: '_root',
   keepFiles: [],
 };
 
-function getPubDir(srcDir, pubDirName) {
-  try {
-    const pubDir = join(srcDir, pubDirName);
-    const stats = statSync(pubDir);
-    if (!stats.isDirectory()) throw new Error();
-    return pubDir;
-  } catch (err) {
-    pennyLogger.info(`no sub-directory named ${pubDirName} was found; using source directory as web-root`);
-    return srcDir;
-  }
-}
+// function getPubDir(srcDir, pubDirName) {
+//   try {
+//     const pubDir = join(srcDir, pubDirName);
+//     const stats = statSync(pubDir);
+//     if (!stats.isDirectory()) throw new Error();
+//     return pubDir;
+//   } catch (err) {
+//     pennyLogger.info(`no sub-directory named ${pubDirName} was found; using source directory as web-root`);
+//     return srcDir;
+//   }
+// }
 
 function init(srcDir, doSomething) {
   configExplorer
@@ -37,7 +37,19 @@ function init(srcDir, doSomething) {
     .then((result) => {
       const options = Object.assign({}, defaults, result ? result.config : {});
       eazyLogger.setLevel(options.logLevel);
-      const pubDir = getPubDir(srcDir, options.pubDirName);
+      let pubDir = srcDir;
+      if ('pubDirName' in options) {
+        const { pubDirName } = options;
+        try {
+          pubDir = join(srcDir, options.pubDirName);
+          const stats = statSync(pubDir);
+          if (!stats.isDirectory()) throw new Error();
+        } catch (err) {
+          pennyLogger.info(`no sub-directory named ${pubDirName} was found; using source directory as web-root`);
+          pubDir = srcDir;
+        }
+      }
+      // const pubDir = getPubDir(srcDir, options.pubDirName);
       return [pubDir, options];
     })
     .then(doSomething)
@@ -46,7 +58,7 @@ function init(srcDir, doSomething) {
 
 function serve(srcDir) {
   init(srcDir, ([pubDir, options]) => {
-    console.log({pubDir});
+    // console.log({pubDir});
     Object.assign(options, { isDev: true, isBuild: false});
     // eazyLogger.setLevel(options.logLevel);
     // const pubDir = getPubDir(srcDir, options.pubDirName);
@@ -56,6 +68,7 @@ function serve(srcDir) {
 
 function build(srcDir, outDir) {
   init(srcDir, ([pubDir, options]) => {
+    // console.log({pubDir});
     Object.assign(options, { isDev: process.env.NODE_ENV == 'development', isBuild: true});
     // eazyLogger.setLevel(options.logLevel);
     // const pubDir = getPubDir(srcDir, options.pubDirName);
