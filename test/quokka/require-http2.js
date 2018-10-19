a49d6d1782acd723d0ab615e9af08dce9b73d2d6
Lu Nelson <lunelson@gmail.com>
2018-10-19 13:43:50 +0200

    do it with require-resolve pkg

--- test/quokka/require-http2.js
+++ test/quokka/require-http2.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const requireResolve = require('resolve'); // for implementing require in put
 
 __dirname; //?
-require('../../node_modules/http2'); //?
 path.resolve('../../node_modules/http2', __dirname); //?
 path.resolve(__dirname, '../../node_modules/http2'); //?
-require(path.resolve(__dirname, '../../node_modules/http2')); //?
+
+// require('../../node_modules/http2'); //?
+// require(path.resolve(__dirname, '../../node_modules/http2')); //?
+requireResolve.sync('http2', { basedir: __dirname }); //?
