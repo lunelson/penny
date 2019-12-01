87346557bdab073e473d4e751f3b22b410582689
Lu Nelson <lunelson@gmail.com>
2019-12-01 13:41:22 +0100

    package updates and fixes

--- test/quokka/scss-filter-test.js
+++
@@ -1,8 +0,0 @@
-const Sass = require('node-sass');
-
-const test = Sass.renderSync({
-  data: `$color: red;`,
-  file: 'body { background-color: $color; }',
-}).css;
-
-console.log(test.toString());
--- test/quokka/url-pattern-test.js
+++ test/quokka/url-pattern-test.js
@@ -1,16 +1,3 @@
-// OBJECT REST/SPREAD
-const alpha = {a: 1};
-const beta = {b: 3};
-console.log(JSON.stringify({...alpha, ...beta}, null, 2));
-
-// DEBOUNCE
-const _ = require('lodash');
-const foo = _.debounce((str) => console.log(str), 100);
-foo('hi 1');
-foo('hi 2');
-foo('hi 3');
-foo('hi 4');
-
 // URL-PATTERN vs ANYMATCH
 // .e.g matchRoute vs matchGlob
 /*
@@ -20,7 +7,7 @@ foo('hi 4');
     href
 */
 const $pages = new Map();
-$pages.match = function(pattern){
+$pages.match = function(pattern) {
   // const pat = new UrlPattern(`${pattern}(.html)`);
   return Array.from(this.entries()).map(entry => {
     const match = new UrlPattern(`${pattern}(.html)`).match(entry[0]);
@@ -44,6 +31,3 @@ Array.from($pages.entries()).map(entry => {
 $pages.match('/posts/*');//?
 p1.match('/posts/index.html');//?
 p1.match('/posts/nested/index.html');//?
-
-// QS
-const qs = require('qs');
